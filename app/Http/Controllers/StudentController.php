c05873b7bcb5940bd7135cf693af0aab6137ef53
hendisantika <hendisantika@yahoo.co.id>
2023-11-12 18:30:30 +0700

    #7 - Add index Student ✏️📗:octocat:🐧🐳

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
@@ -11,7 +12,10 @@ class StudentController extends Controller
      */
     public function index()
     {
-        //
+        $students = Student::latest()->paginate(5);
+
+        return view('students.index', compact('students'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
     }
 
     /**
