adb839c5b3cf298c35a4e4981f26188d127c591b
hendisantika <hendisantika@yahoo.co.id>
2023-11-12 18:35:39 +0700

    #18 - Add destroy Student ✏️📗:octocat:🐧🐳

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -77,8 +77,11 @@ class StudentController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Student $student)
     {
-        //
+        $student->delete();
+
+        return redirect()->route('students.index')
+            ->with('success', 'Student deleted successfully');
     }
 }
